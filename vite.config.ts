3dd53b94d8868985003849a8fb142433a3225f3c
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-05-15 17:44:05 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: true, // enables access from network (0.0.0.0)
+  },
+});
